549c8e278404afee48881c58a9850df83582b7b7
chrysalxm <156919554+fkuruthl@users.noreply.github.com>
2024-07-17 14:23:48 +0400

    Add files via upload

---
+++ intra-uuid-831d6b19-2f3e-4aa6-adfc-8055f67649ee-5423502-fkuruthl/get_next_line_bonus.c
@@ -0,0 +1,112 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkuruthl <fkuruthl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 13:37:02 by fkuruthl          #+#    #+#             */
+/*   Updated: 2024/01/19 13:46:45 by fkuruthl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*read_from_fd(int fd, char *pent_buffer)
+{
+	char	*temp_buff;
+	int		len;
+
+	temp_buff = malloc(sizeof(char) * ((size_t)BUFFER_SIZE + 1));
+	if (!temp_buff)
+		return (NULL);
+	len = 1;
+	while (!ft_strchr(pent_buffer, '\n') && (len != 0))
+	{
+		len = read(fd, temp_buff, BUFFER_SIZE);
+		if (len < 0)
+		{
+			free(temp_buff);
+			free(pent_buffer);
+			return (NULL);
+		}
+		temp_buff[len] = '\0';
+		pent_buffer = ft_strjoin(pent_buffer, temp_buff);
+	}
+	free(temp_buff);
+	return (pent_buffer);
+}
+
+char	*skip_duplicate(char *pent_buffer)
+{
+	int		i;
+	int		j;
+	char	*new_buffer;
+
+	i = 0;
+	while (pent_buffer[i] != '\0' && pent_buffer[i] != '\n')
+		i++;
+	if (pent_buffer[i] == '\n')
+		i++;
+	if (pent_buffer[i] == '\0')
+	{
+		free(pent_buffer);
+		return (NULL);
+	}
+	new_buffer = malloc(ft_strlen(pent_buffer) - i + 1);
+	if (!new_buffer)
+		return (NULL);
+	j = 0;
+	while (pent_buffer[i] != '\0')
+		new_buffer[j++] = pent_buffer[i++];
+	new_buffer[j] = '\0';
+	free(pent_buffer);
+	return (new_buffer);
+}
+
+char	*ft_line(char *str)
+{
+	int		i;
+	char	*new_line;
+
+	i = 0;
+	if (!*str)
+		return (NULL);
+	while (str[i] != '\0' && str[i] != '\n')
+		i++;
+	new_line = (char *)malloc((i + 2));
+	if (!new_line)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0' && str[i] != '\n')
+	{
+		new_line[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		new_line[i] = str[i];
+		i++;
+	}
+	new_line[i] = '\0';
+	return (new_line);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*final_line;
+	static char	*pent_buffer[10240];
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE > 2147483647)
+	{
+		return (NULL);
+	}
+	pent_buffer[fd] = read_from_fd(fd, pent_buffer[fd]);
+	if (!pent_buffer[fd])
+	{
+		return (NULL);
+	}
+	final_line = ft_line(pent_buffer[fd]);
+	pent_buffer[fd] = skip_duplicate(pent_buffer[fd]);
+	return (final_line);
+}
---
+++ intra-uuid-831d6b19-2f3e-4aa6-adfc-8055f67649ee-5423502-fkuruthl/get_next_line_utils_bonus.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fkuruthl <fkuruthl@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/05 13:34:12 by fkuruthl          #+#    #+#             */
+/*   Updated: 2024/01/17 17:11:00 by fkuruthl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+	{
+		return (0);
+	}
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char)c)
+			return ((char *)str + i);
+		i++;
+	}
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*temp;
+
+	i = 0;
+	j = 0;
+	if (!s1)
+		s1 = new_temp();
+	if (!s2)
+		return (NULL);
+	temp = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!temp)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		temp[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		temp[i++] = s2[j++];
+	}
+	temp[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
+	return (temp);
+}
+
+char	*new_temp(void)
+{
+	char	*str;
+
+	str = malloc(1);
+	if (!str)
+	{
+		return (NULL);
+	}
+	str[0] = '\0';
+	return (str);
+}
